996011d9ebdc02c13bbedfc06a8ef420f6f5c081
edrickmanoel <edrickmanoel@nasajon.com.br>
2019-02-13 07:40:05 -0200

    changes

--- lib/src/login_auto_agendamento/login_auto_agendamento_component.dart
+++ lib/src/login_auto_agendamento/login_auto_agendamento_component.dart
@@ -5,19 +5,19 @@ import 'package:angular_components/utils/browser/window/module.dart';
 import 'package:angular_components/material_input/material_input.dart';
 
 @Component(
-  selector: 'agendamento-app',
-  templateUrl: 'agendamento_component.html',
+  selector: 'login-auto-agendamento-app',
+  templateUrl: 'login_auto_agendamento_component.html',
   directives: const [
     coreDirectives,
     materialInputDirectives,
     MaterialButtonComponent],
   providers: const [materialProviders, windowBindings, datepickerBindings],
   styleUrls: const [
-    'agendamento_component.scss.css',
+    'login_auto_agendamento_component.scss.css',
     'package:angular_components/app_layout/layout.scss.css'
   ],
 )
-class AgendamentoComponent {
+class LoginAutoAgendamentoComponent {
   String email = '';
   String password = '';
     String error;
