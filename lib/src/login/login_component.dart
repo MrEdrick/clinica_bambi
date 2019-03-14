99d79983a15e6487d5d153d9a3a04a4fb1a4a092
edrickmanoel <edrickmanoel@nasajon.com.br>
2019-03-14 06:47:22 -0300

    changes

--- lib/src/login/login_component.dart
+++ lib/src/login/login_component.dart
@@ -46,7 +46,7 @@ class LoginComponent extends Object implements OnActivate {
   }
   
   void onGetInside() async {
-    error = await new AuthApp().registerUser(email, password);
+    error = await new AuthApp().login(email, password);
     if (error == '') {
       goDeshboardAgendamento();
     } else {
