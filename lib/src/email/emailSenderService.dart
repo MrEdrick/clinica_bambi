e5ab8ca6db331cb15a733609daf0dd272bdb4887
edrickmanoel <edrickmanoel@nasajon.com.br>
2019-04-05 21:27:13 -0300

    changes

--- lib/src/email/emailSenderService.dart
+++ lib/src/email/emailSenderService.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'email.dart';
 import 'emailSender.dart';
 import 'package:http/browser_client.dart';
+import 'package:http/http.dart';
 import 'dart:convert';
 //import 'dart:html';
 //import 'package:googleapis/gmail/v1.dart';
@@ -19,31 +20,19 @@ class EmailSenderService {
   "message": "Esta é a mensage de teste de e-mail pela API",
   "toAddresses": "clinicaodontologicabambi@gmail.com",
   "source": "clinicaodontologicabambi@gmail.com"};
+  Response uriResponse;
 
 
     try {
-      var uriResponse = await new BrowserClient().post('https://us-east-1.console.aws.amazon.com/apigateway/home?region=us-east-1#/apis/mz8uki4o3l/resources/dh84j1o5ge/sendEmail',
-          body: data);
-      print(uriResponse);
+      uriResponse = await new BrowserClient().post(
+        //'https://us-east-1.console.aws.amazon.com/apigateway/home?region=us-east-1#/apis/mz8uki4o3l/resources/dh84j1o5ge/sendEmail',
+        'https://mz8uki4o3l.execute-api.us-east-1.amazonaws.com/default/sendEmail',
+        headers: {},
+        body: jsonEncode(data));
     } finally {
-
+      print(uriResponse.toString());
     }
     
     return new EmailSender({}, '', email);
-    //String url = 'https://www.googleapis.com/gmail/v1/users/' + email.receiver + '/messages/send';
-
-    /*GoogleSignIn googleSignIn = new GoogleSignIn(
-      scopes: <String>[
-        'https://www.googleapis.com/auth/gmail.send'
-      ],
-    );*/
-
-    /*await googleSignIn.signIn().then((data) {
-      data.authHeaders.then((result) {
-        Map header = {'Authorization': result['Authorization'], 'X-Goog-AuthUser': result['X-Goog-AuthUser'],
-                      'Accept': 'application/json', 'Content-type': 'application/json'};
-        return new EmailSender(header, url, email);
-      });                          
-    });*/
   }
 }
