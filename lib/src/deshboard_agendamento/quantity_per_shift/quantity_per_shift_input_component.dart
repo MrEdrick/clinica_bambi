446791287fc7f1ebcf5de08e77bf8e224f9ad443
edrickmanoel <edrickmanoel@nasajon.com.br>
2019-04-22 11:24:37 -0300

    changes

--- lib/src/deshboard_agendamento/quantity_per_shift/quantity_per_shift_input_component.dart
+++ lib/src/deshboard_agendamento/quantity_per_shift/quantity_per_shift_input_component.dart
@@ -1,58 +1,44 @@
-import 'dart:html';
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
-import 'package:angular_components/material_dialog/material_dialog.dart';
-import 'package:angular_components/laminate/components/modal/modal.dart';
-import 'package:angular_components/auto_dismiss/auto_dismiss.dart';
 import 'package:angular_components/angular_components.dart';
-import 'package:angular_components/material_button/material_button.dart';
-import 'package:angular_components/material_icon/material_icon.dart';
 import 'package:angular_components/material_input/material_input.dart';
 import 'package:angular_components/material_datepicker/module.dart';
 import 'package:angular_components/utils/browser/window/module.dart';
-import 'package:angular_components/material_select/material_dropdown_select.dart';
-import 'package:angular_components/material_select/material_dropdown_select_accessor.dart';
-
-import '../../agendamento/procedure/procedure_service.dart';
-import '../../agendamento/procedure/procedure_constants.dart';
-
-import '../../firebase/firestore.dart';
-
 
 import '../../agendamento/user/user_service.dart';
 
 @Component(
-    selector: 'procedure-edit-app',
+    selector: 'quantity_per_shift_input_component',
     styleUrls: const [
-      'procedure_edit_component.scss.css',
+      'quantity_per_shift_input_component.scss.css',
       'package:angular_components/app_layout/layout.scss.css'
     ],
-    templateUrl: 'procedure_edit_component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    templateUrl: 'quantity_per_shift_input_component.html',
     directives: const [
       coreDirectives,
       formDirectives,
-      AutoFocusDirective,
-      MaterialIconComponent,
-      MaterialButtonComponent,
       MaterialInputComponent,
       materialInputDirectives,
-      MaterialDropdownSelectComponent,
-      DropdownSelectValueAccessor,
-      MaterialDialogComponent,
-      ModalComponent,
-      AutoDismissDirective,
     ],
     providers: [
       windowBindings,
       datepickerBindings
     ])
 class QuantityPerShiftComponent implements OnInit {
+  final ChangeDetectorRef _changeDetectorRef; 
+
+  @Input()
+  String shift = '';
 
   String quantity = '';
-  String shift = '';
+
+  QuantityPerShiftComponent(this._changeDetectorRef);
 
   void ngOnInit() {
     if (new UserService().user  == null)
       return;
+
+    _changeDetectorRef.markForCheck();
   }
 }
