713a65febf758922e23d2f8e2b2461e5a0465169
edrickmanoel <edrickmanoel@nasajon.com.br>
2019-04-23 09:22:19 -0300

    changes

--- lib/src/agendamento/dentist_procedure/dentist_procedure_service.dart
+++ lib/src/agendamento/dentist_procedure/dentist_procedure_service.dart
@@ -38,14 +38,14 @@ class DentistProcedureService {
     return _list;
   }
 
-  Future<DentistProcedure> getDentistProcedureByFilter(Map filter) async {
+  Future<DentistProcedure> getOneDentistProcedureByFilter(Map filter) async {
     Map doc;
 
     if ((_dentistProcedureList == null) || (_dentistProcedureList.length == 0)) {
       await getAllDentistProcedureAcives();
     }
 
-    doc = _dentistListById[id];
+    doc = getDentistProcedureListWithFilterFromList(filter).first;
 
     if (doc == null) {
       doc = (await new DentistProcedureDAO()
@@ -55,6 +55,59 @@ class DentistProcedureService {
 
     return turnMapInDentistProcedure(doc);
   }
+
+List<Map> getDentistProcedureListWithFilterFromList(Map filter) {
+    List<Map> _listDocumentSnapshot = new List<Map>();
+
+    List<Map> _listDocumentSnapshotTemp = new List<Map>();
+
+    void ListsApplyFilter() {
+      if (_listDocumentSnapshotTemp.length > 0) {
+        _listDocumentSnapshotTemp.forEach((doc) {
+          _listDocumentSnapshot.add(new Map.from(doc));
+        });
+
+        _listDocumentSnapshotTemp.clear();
+      }
+    }
+
+    _listDocumentSnapshot = _dentistProcedureList;
+
+    _listDocumentSnapshotTemp.clear();
+
+    if ((filter["dentistId"] != null) && (filter["dentistId"] != '')) {
+      _listDocumentSnapshot.forEach((doc) {
+        if (doc["dentistId"].toString() == filter["dentistId"].toString()) {
+          _listDocumentSnapshotTemp.add(new Map.from(doc));
+        }
+      });
+    }
+
+    if ((filter["dentistId"] != null) && (filter["dentistId"] != '')) {
+      _listDocumentSnapshot.clear();
+    }
+
+    ListsApplyFilter();
+
+    if ((filter["procedureId"] != null) && (filter["procedureId"] != '')) {
+      _listDocumentSnapshot.forEach((doc) {
+        if (doc["procedureId"].toString() == filter["procedureId"].toString()) {
+          _listDocumentSnapshotTemp.add(new Map.from(doc));
+        }
+      });
+    }
+
+    if ((filter["procedureId"] != null) && (filter["procedureId"] != '')) {
+      _listDocumentSnapshot.clear();
+    }
+
+    ListsApplyFilter();
+
+    _dentistProcedureListWithFilter = _listDocumentSnapshot;
+    
+    return _dentistProcedureListWithFilter;
+  }
+
 
   DentistProcedure turnMapInDentistProcedure(Map map) {
     return new DentistProcedure(map["documentPath"], map["dentistId"], map["procedureId"]);
