80d03a4c5d17888e924096c20f100435e7460771
edrickmanoel@nasajon.com.br <edrickmanoel@nasajon.com.br>
2019-08-23 18:06:27 -0300

    changes

--- lib/src/email_fale_conosco/email_fale_conosco_component.dart
+++ lib/src/email_fale_conosco/email_fale_conosco_component.dart
@@ -38,16 +38,25 @@ import '../email/emailSenderHTTP.dart';
 )
 
 class EmailFaleConoscoComponent {
+  final ChangeDetectorRef _changeDetectorRef;
+
   bool showAssertMessageEmailNotSended = false;
   bool showAssertMessageEmailSended = false;
+  bool showAssertCannotSend = false;
   bool multiline = true;
+  bool disabled = false;
 
   String email = '';
+  String name = '';
   String subject = '';
-  String mensage = '';
+  String message = '';
+  String sendButtonMessage = "Enviar";
 
   Response response;  
   EmailSenderHTTP emailSenderHTTP;
+
+  EmailFaleConoscoComponent(this._changeDetectorRef);
+
 
   void clickClose() {
     onDismissAssertMessage();
@@ -59,12 +68,42 @@ class EmailFaleConoscoComponent {
   void onDismissAssertMessage() {
     showAssertMessageEmailNotSended = false;
     showAssertMessageEmailSended = false;
+    showAssertCannotSend = false;
+  }
+
+  bool asserts() {
+    if ((name.isEmpty) || (email.isEmpty) || (subject.isEmpty) ||
+        (message.isEmpty)) {
+      return false;
+    }
+
+    return true;
+  }
+
+  void clearFields() {
+    email = "";
+    name = "";
+    subject = "";
+    message = "";
   }
 
   void clickSend() async {
+    if (disabled) {
+      return;
+    }
+
+    if (!asserts()) {
+      showAssertCannotSend = true;
+      return;
+    }
+
+    disabled = true;
+    sendButtonMessage = "Enviando...";
+    _changeDetectorRef.markForCheck();
+
     emailSenderHTTP = await new EmailSenderService(
       new Email(CLINIC_EMAIL, 
-                CLINIC_EMAIL, subject, mensage, 
+                CLINIC_EMAIL, subject + ' - De Nome: ' + name + ' Email: ' + email, message, 
                 null, null)
     ).emailSenderAmazon();
 
@@ -73,7 +112,7 @@ class EmailFaleConoscoComponent {
       emailSenderHTTP = await new EmailSenderService(
         new Email(CLINIC_EMAIL, 
                   email, subject, 
-                  ' Olá, recebemos seu e-mail. Esta é apenas uma mensagem automática, por favor não responda.' + '\r\n' + mensage, 
+                  ' Olá, recebemos seu e-mail. Esta é apenas uma mensagem automática, por favor não responda.' + '\r\n' + message, 
                   null, null)
       ).emailSenderAmazon();
 
@@ -83,7 +122,12 @@ class EmailFaleConoscoComponent {
     if (response.statusCode != 200) {
       showAssertMessageEmailNotSended = true;
     } else {
+      clearFields();
       showAssertMessageEmailSended = true;
     }
+
+    disabled = false;
+    sendButtonMessage = "Enviar";
+    _changeDetectorRef.markForCheck();
   }
 }
