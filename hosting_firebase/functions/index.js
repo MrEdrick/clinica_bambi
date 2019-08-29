56b7edc516d4baf6f70f40dd8cb8114233a73c7e
edrickmanoel@nasajon.com.br <edrickmanoel@nasajon.com.br>
2019-08-29 14:59:00 -0300

    changes

--- hosting_firebase/functions/index.js
+++ hosting_firebase/functions/index.js
@@ -1,11 +1,15 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const nodemailer = require('nodemailer');
-const cors = require('cors')({origin: true});
+const cors = require('cors')({ origin: true });
+const axios = require('axios');
 
-const name = 'Clinica Bambi';
-const email = 'clinicaodontologicabambi@gmail.com';
-const password = 'B715706**';
+const NAME = 'Clinica Bambi';
+const EMAIL = 'clinicaodontologicabambi@gmail.com';
+const PASSWORD = 'B715706**';
+const AMAZON_API_ENDPONT_SEND_EMAIL = 'https://mz8uki4o3l.execute-api.us-east-1.amazonaws.com/default/sendEmail';
+const GMAIL_API_ENDPONT_SEND_EMAIL = 'https://us-central1-bambi-210400.cloudfunctions.net/sendMail';
+
 
 admin.initializeApp();
 
@@ -15,38 +19,46 @@ admin.initializeApp();
 let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: email,
-        pass: password
+        user: EMAIL,
+        pass: PASSWORD
     }
 });
 
 exports.sendMail = functions.https.onRequest((req, res) => {
     cors(req, res, () => {
-      
+
         // getting dest email by query string
         const dest = req.query.dest;
         const subject = req.query.subject;
         const html = JSON.parse(req.body).message;
 
         const mailOptions = {
-            from: name + ' <' + email + '>',
+            from: NAME + ' <' + EMAIL + '>',
             to: dest,
             subject: subject, // email subject
             html: html// email content in HTML
         };
-  
+
         // returning result
         return transporter.sendMail(mailOptions, (erro, info) => {
-            if(erro){
+            if (erro) {
                 return res.send(erro.toString());
             }
             return res.send('Sended');
         });
-    });    
+    });
 });
 
-/*exports.scheduledFunctionRemember = functions.pubsub.schedule('5 11 * * *')
-  .timeZone('America/New_York') // Users can choose timezone - default is America/Los_Angeles
-  .onRun((context) => {
-  console.log('This will be run every day at 11:05 AM Eastern!');
-});*/
+exports.scheduledFunctionRemember = functions.pubsub.schedule('every day 08:00')
+    .timeZone('America/Sao_Paulo') // Users can choose timezone - default is America/Los_Angeles
+    .onRun((context) => {
+        axios({
+            method: 'post',
+            url: '/user/12345',
+            data: {
+                firstName: 'Fred',
+                lastName: 'Flintstone'
+            }
+        });
+        console.log('This will be run every day at 11:05 AM Eastern!');
+    });
