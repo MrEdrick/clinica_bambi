53d7032f3e6b448bbe668435f491864b8bbf46a5
edrickmanoel@nasajon.com.br <edrickmanoel@nasajon.com.br>
2019-08-30 16:36:10 -0300

    changes

--- hosting_firebase/functions/index.js
+++ hosting_firebase/functions/index.js
@@ -78,20 +78,27 @@ exports.scheduledFunctionRemember = functions.pubsub.schedule('0 8 * * *')
                         EMAIL,
                         'edrickmanoel@nasajon.com.br',
                         'Lembre de consulta marcada',
-                        `<div 
-                            style=" font-family:Arial, Helvetica, sans-serif; 
-                            font-size: 22px; 
-                            font-weight: 600;
-                            color:#666666;">
-                           Olá, tudo certo?
-                        </div>
-                        <div 
-                            style=" font-family:Arial, Helvetica, sans-serif; 
-                            font-size: 16px; 
-                            font-weight: 600;
-                            color:#888888;">
-                            Gostariamos de lembrar que hoje nós temos uma consulta marcada.
-                       </div>`);
+                        `<html>
+                            <head>
+                            <meta charset="utf-8">
+                            </head>
+                            <body>
+                                <div 
+                                    style=" font-family:Arial, Helvetica, sans-serif; 
+                                    font-size: 22px; 
+                                    font-weight: 600;
+                                    color:#666666;">
+                                Olá, tudo certo?
+                                </div>
+                                <div 
+                                    style=" font-family:Arial, Helvetica, sans-serif; 
+                                    font-size: 16px; 
+                                    font-weight: 600;
+                                    color:#888888;">
+                                    Gostariamos de lembrar que hoje nós temos uma consulta marcada.
+                            </div>
+                        </body>
+                       </html>`);
                 });
                 return console.log("Sended");
             })
