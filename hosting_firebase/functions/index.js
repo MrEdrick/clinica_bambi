1d11ff86de8ea7bdf4df07a63b0b4595d825d39f
edrickmanoel@nasajon.com.br <edrickmanoel@nasajon.com.br>
2019-08-30 13:05:58 -0300

    changes

--- hosting_firebase/functions/index.js
+++ hosting_firebase/functions/index.js
@@ -1,28 +1,56 @@
+const firebase = require('firebase');
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const nodemailer = require('nodemailer');
 const cors = require('cors')({ origin: true });
-const axios = require('axios');
 
 const NAME = 'Clinica Bambi';
 const EMAIL = 'clinicaodontologicabambi@gmail.com';
 const PASSWORD = 'B715706**';
 const AMAZON_API_ENDPONT_SEND_EMAIL = 'https://mz8uki4o3l.execute-api.us-east-1.amazonaws.com/default/sendEmail';
 const GMAIL_API_ENDPONT_SEND_EMAIL = 'https://us-central1-bambi-210400.cloudfunctions.net/sendMail';
-
+const APPOINTMENT_SCHEDULING_COLLECTION = 'appointmentsScheduling';
+const APPOINTMENT_SCHEDULING_DATE_APPOINTMENT_SCHEDULING_FIELD = 'dateAppointmentScheduling';
 
 admin.initializeApp();
 
 /**
 * Here we're using Gmail to send 
 */
-let transporter = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: EMAIL,
         pass: PASSWORD
     }
 });
+
+const firebaseConfig = {
+    apiKey: "AIzaSyATOL9trCxr-LOmQDsvhgnLl36YZ61IQn4",
+    authDomain: "bambi-210400.firebaseapp.com",
+    databaseURL: "https://bambi-210400.firebaseio.com",
+    projectId: "bambi-210400",
+    storageBucket: "bambi-210400.appspot.com",
+    messagingSenderId: "1046590420458",
+    appId: "1:1046590420458:web:03d67dfa9cab2693"
+};
+
+function sendEmail(name, email, to, subject, html) {
+    const mailOptions = {
+        from: email + ' <' + name + '>',
+        to: to,
+        subject: subject, // email subject
+        html: html// email content in HTML
+    };
+
+    // returning result
+    return transporter.sendMail(mailOptions, (erro, info) => {
+        if (erro) {
+            return res.send(erro.toString());
+        }
+        return res.send('Sended');
+    });
+}
 
 exports.sendMail = functions.https.onRequest((req, res) => {
     cors(req, res, () => {
@@ -32,32 +60,28 @@ exports.sendMail = functions.https.onRequest((req, res) => {
         const subject = req.query.subject;
         const html = JSON.parse(req.body).message;
 
-        const mailOptions = {
-            from: NAME + ' <' + EMAIL + '>',
-            to: dest,
-            subject: subject, // email subject
-            html: html// email content in HTML
-        };
-
         // returning result
-        return transporter.sendMail(mailOptions, (erro, info) => {
-            if (erro) {
-                return res.send(erro.toString());
-            }
-            return res.send('Sended');
-        });
+        return sendEmail(NAME, EMAIL, dest, subject, html);
     });
 });
 
 exports.scheduledFunctionRemember = functions.pubsub.schedule('0 8 * * *')
     .timeZone('America/Sao_Paulo') // Users can choose timezone - default is America/Los_Angeles
     .onRun((context) => {
-        axios.post(({
-            method: 'POST',
-            //url: GMAIL_API_ENDPONT_SEND_EMAIL + '?' + 'dest=' + email.receiver + '&' + 'subject=' + email.subject,
-            url: GMAIL_API_ENDPONT_SEND_EMAIL + '?' + 'dest=edrickmanoel@nasajon.com.br'  + '&' + 'subject=TesteAgenda',
-            data: {
-                message: 'Teste 123',
-            }
-        });
+        firebase.initializeApp(firebaseConfig);
+        var db = firebase.firestore();
+        db.collection(APPOINTMENT_SCHEDULING_COLLECTION).where(
+            APPOINTMENT_SCHEDULING_DATE_APPOINTMENT_SCHEDULING_FIELD, "==",
+            new Date().toJSON().slice(0, 10).replace(/-/g, '-'))
+            .get()
+            .then(function (querySnapshot) {
+                querySnapshot.forEach(function (doc) {
+                    // doc.data() is never undefined for query doc snapshots
+                    sendEmail(NAME, EMAIL, 'edrickmanoel@nasajon.com.br', 'Teste Scheduler', 'Teste Message');
+                });
+                return console.log("Sended"); 
+            })
+            .catch(function(error) {
+                return console.log("Error getting documents: ", error);
+            });
     });
