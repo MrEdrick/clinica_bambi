f139a3670001a8d1a897ef100fa5e37a3438674b
edrickmanoel@nasajon.com.br <edrickmanoel@nasajon.com.br>
2019-09-06 14:34:55 -0300

    changes

--- lib/src/appointment/base/collection.dart
+++ lib/src/appointment/base/collection.dart
@@ -4,5 +4,12 @@ class Collection {
   String _description;
   List<Field> _fieldList;
 
-  Collection(this._description, this._fieldList)
+  Collection(this._description, this._fieldList);
+
+  String get description => _description;
+  set description(String description) => _description = description;
+
+  List<Field> get fieldList => _fieldList;
+  set fieldList(List<Field> fieldList) => _fieldList = fieldList;
+
 }
--- lib/src/appointment/base/field.dart
+++ lib/src/appointment/base/field.dart
@@ -3,4 +3,11 @@ class Field {
   String _type;
 
   Field(this._description, this._type); 
+
+  String get description => _description;
+  set description(String description) => _description = description;
+
+  String get type => _type;
+  set type(String type) => _type = type;
+
 }
