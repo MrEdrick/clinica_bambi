350652814d878adc302e58df98448571c2ce6686
edrickmanoel@nasajon.com.br <edrickmanoel@nasajon.com.br>
2019-09-06 16:22:19 -0300

    changes

--- lib/src/appointment/base/filter.dart
+++ lib/src/appointment/base/filter.dart
@@ -2,9 +2,20 @@ import 'collection.dart';
 
 class Filter {
   Collection _collection;
+  Map<String, List<String>> _fieldValueCompationMap;
+  Map<String, String> _orderBy;
 
-  Filter(this._collection);
+  Filter(this._collection, this._fieldValueCompationMap, this._orderBy);
 
   Collection get collection => _collection;
   set collection(Collection collection) => _collection = collection;
+
+  Map<String, List<String>> get fieldValueCompationMap =>
+      _fieldValueCompationMap;
+  set fieldValueCompationMap(
+          Map<String, List<String>> fieldValueCompationMap) =>
+      _fieldValueCompationMap = fieldValueCompationMap;
+
+  Map<String, String> get orderBy => _orderBy;
+  set orderBy(Map<String, String> orderBy) => _orderBy = orderBy;
 }
