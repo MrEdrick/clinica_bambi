d700eff82932f7c253dad9c280bef9fb3910c039
edrickmanoel@nasajon.com.br <edrickmanoel@nasajon.com.br>
2019-09-09 14:45:27 -0300

    changes

--- lib/src/appointment/agreement/agreement_dao.dart
+++ lib/src/appointment/agreement/agreement_dao.dart
@@ -4,8 +4,7 @@ import 'agreement_constants.dart';
 class AgreementDAO extends DAO {
   AgreementDAO();
 
-  @override
-  Future<List<Map>> getAllFilter(
-          Map filter, Map orderBy, List comparisons) async =>
-      super.getAllFilter(AGREEMENT_COLLECTION, filter, orderBy, comparisons);
+  //Future<List<Map>> getAllFilter(
+  //        Map filter, Map orderBy, List comparisons) async =>
+  //    super.getAllFilter(AGREEMENT_COLLECTION, filter, orderBy, comparisons);
 }
