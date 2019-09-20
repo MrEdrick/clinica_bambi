df55b55e2c8643b6a43a71a6b428bebb1f617d98
edrickmanoel@nasajon.com.br <edrickmanoel@nasajon.com.br>
2019-09-20 18:42:50 -0300

    changes

--- lib/src/appointment/core/controller/service.dart
+++ lib/src/appointment/core/controller/service.dart
@@ -1,5 +1,6 @@
-import '../dao/filter.dart';
 import '../dao/dao.dart';
+import '../dao/condition.dart';
+import '../dao/order_by.dart';
 
 class Service {
   static String _id;
@@ -33,4 +34,29 @@ class Service {
 
     return _mapList;
   }
+
+  Future<Map> getById(String id) async {
+    Map doc;
+
+    if (id.isEmpty) {
+      return null;
+    }
+
+    if ((_mapList == null) || (_mapList?.length == 0)) {
+      await getAllAcives();
+    }
+
+    doc = _mapListById[id];
+
+    if (doc == null) {
+      _dao.filter.conditionList.clear()
+      _dao.filter.conditionList.add(new Condition("id", id, "="));
+      _dao.filter.orderBy = new OrderBy("description", "asc");
+      doc = (await new _dao.getAllFilter())
+          .first;
+    }
+
+    return doc;
+  }
+
 }
